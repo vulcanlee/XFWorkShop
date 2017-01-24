4d64f1fe2915a3db9ac3c8ef93ed4d79ae22b9bc
李進興 Vulcan <vulcan@miniasp.com>
2017-01-25 02:16:13 +0800

    多國語言應用程式設計

---
+++ XFLocalization/XFLocalization/XFLocalization/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XFLocalization.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
